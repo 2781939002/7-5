e246ab66c133c2b9e7daa5f4cff547174eaf2b84
2781939002 <98596204+2781939002@users.noreply.github.com>
2022-07-18 20:05:28 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -341,10 +341,10 @@ function shareText(score) {
         return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
     }
     SubmitResults();
-    if (score <= 99) return '犹豫就会败北！';
-    if (score <= 149) return '忍杀';
-    if (score <= 199) return '修';
-    return '罗！';
+    if (score <= 99) return '必定遭报应！';
+    if (score <= 149) return '欺负';
+    if (score <= 199) return '欺';
+    return '付！';
 }
 
 function toStr(obj) {
